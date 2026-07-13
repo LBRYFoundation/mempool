1609088ff1cf05bc2e0ebe7448ee8a3ab09bfd33
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2026-07-13 16:17:54 +0200

    Fix replacement

--- frontend/src/app/shared/coin.constants.ts
+++ frontend/src/app/shared/coin.constants.ts
@@ -1,4 +1,4 @@
 export const COIN_TICKER = 'LBC';
-export const COIN_SUBUNIT_NAME = 'dewey';
+export const COIN_SUBUNIT_NAME = 'deweys';
 export const COIN_TO_SUBUNIT_MULTIPLIER = 100_000_000;
 export const COIN_MAX_SUPPLY = 1_000_000_000;
